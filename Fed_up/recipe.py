9b876b722b5b9211c6f2480d818a7a2c380faafd
Nuno Loureiro <nunoloureiro88@gmail.com>
2020-11-17 10:08:37 +0000

    Merging cleaning data

--- Fed_up/recipe.py
+++ Fed_up/recipe.py
@@ -27,28 +27,44 @@ def select_universe(df):
     # load recipes and reviews and merge both tables and grouping them
     # also dropping NAN
     csv_path = os.path.join(os.path.dirname(__file__), "data/raw")
-    recipes_df = get_raw_data()
-    recipes_df.rename(columns={"id":"recipe_id"}, inplace=True)
+    recipes_df = df
+    recipes_df.rename(columns={"id": "recipe_id"}, inplace=True)
     reviews_df = pd.read_csv(f'{csv_path}/RAW_interactions.csv')
     merged_df = recipes_df.merge(reviews_df, on="recipe_id", how="inner")
-    review_merge_df = merged_df.groupby(by="name").agg({"recipe_id":"first","minutes":"first","contributor_id":"first","submitted":"first","tags":"first","nutrition":"first","steps":"first", "description":"first", "ingredients": "first", "n_ingredients":"first", "rating":"mean"}).dropna()
+    review_merge_df = merged_df.groupby(by="name") \
+                               .agg({"recipe_id": "first", "minutes": "first", "contributor_id": "first", \
+                                     "submitted": "first", "tags": "first", "nutrition": "first", "steps": "first", \
+                                     "description": "first", "ingredients": "first", "n_ingredients": "first", \
+                                     "rating": "mean"}).dropna()
     #removing very specific ingredients
     #1st i
     print("creating lists for filtering ......")
     ingredients = review_merge_df["ingredients"]
-    ingredients_list_clean = []
-    ingredients_list_for_counting = []
+    # ingredients_list_clean = []
+    # ingredients_list_for_counting = []
+    # for i in ingredients:
+    #     for j in i:
+    #         if j not in ingredients_list_clean:
+    #             ingredients_list_clean.append(j)
+    #         ingredients_list_for_counting.append(j)
+
+    ingredients_dict = {}
     for i in ingredients:
         for j in i:
-            if j not in ingredients_list_clean:
-                ingredients_list_clean.append(j)
-            ingredients_list_for_counting.append(j)
+            if j not in ingredients_dict.keys():
+                ingredients_dict[j] = 1
+            else:
+                ingredients_dict[j] += 1
+
     # now the lowest occurring ingredients are found and assumed as "too specific"
     # Recipes containing these ingredients will be removed
     print("removing too specific ingredients now ......")
 
-    low_use_ingredients = count_occurrence(ingredients_list_clean,ingredients_list_for_counting,2)
+    low_use_ingredients = [i for i, count in ingredients_dict.items() if count >= 2]
+    # low_use_ingredients = count_occurrence(ingredients_list_clean,ingredients_list_for_counting,2)
+
     df_merged_ing = remove_list_from_df(review_merge_df, low_use_ingredients, "ingredients")
+
     print("specific ingredients are now removed")
 
     #removing drinks and icecream recipes using tags:
@@ -80,10 +96,16 @@ def count_occurrence(list_clean,list_tot, threshold):
 
 def remove_list_from_df(df, tag_list, column):
     #removing roew from a dataframe that contain items from a specific tag list
-    data = df
-    for i in tag_list:
-        data = data[~data[column].str.contains(i)]
-    return data
+
+    to_remove = []
+    for tag in tag_list:
+        for index, row in df.iterrows():
+            if tag in row[column]:
+                to_remove.append(index)
+
+    import ipdb; ipdb.set_trace()
+
+    return df[df.loc[index, 'recipe_id'].isin(set(to_remove))]
 
 
 def __clean_nutrition(df, col='nutrition'):
