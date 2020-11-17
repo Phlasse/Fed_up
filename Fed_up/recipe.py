acecbcbf8576d447dc87901474fb8bef4b97dbb3
Nuno Loureiro <nunoloureiro88@gmail.com>
2020-11-17 12:23:28 +0000

    Almost there with the clean data; sample added...

--- Fed_up/recipe.py
+++ Fed_up/recipe.py
@@ -3,6 +3,11 @@
 import os
 import numpy as np
 import pandas as pd
+import ipdb
+import regex as re
+import warnings
+
+from Fed_up import review
 
 
 NUTRITION_COLS = ['calories', 'total_fat', 'sugar', 'sodium',
@@ -11,42 +16,32 @@ NUTRITION_COLS = ['calories', 'total_fat', 'sugar', 'sodium',
 LIST_COLS = ['tags', 'nutrition', 'steps', 'ingredients']
 
 
-def get_raw_data():
+def get_raw_data(folder="data/raw", filename="RAW_recipes.csv"):
     """ Reading recipe data from CSV and evaluating list cols """
-    print('Reading data from CSV and evaluating list cols...')
+    print('Reading recipe data from CSV and evaluating list cols...')
 
     converters = {col: eval for col in LIST_COLS}
-    csv_path = os.path.join(os.path.dirname(__file__), "data/raw")
-    raw_df = pd.read_csv(f'{csv_path}/RAW_recipes.csv', converters=converters)
+    csv_path = os.path.join(os.path.dirname(__file__), folder)
+    raw_df = pd.read_csv(f'{csv_path}/{filename}', converters=converters)
     return raw_df
 
 
 def select_universe(df):
     """ Selecting relevant universe of recipes """
     print('Selecting relevant universe of recipes...')
-    # load recipes and reviews and merge both tables and grouping them
-    # also dropping NAN
+
+    # Load recipes and reviews and merge both tables and grouping them; drop NaN
+    print('> Loading review data for universe selection...')
+    df.rename(columns={"id": "recipe_id"}, inplace=True)
     csv_path = os.path.join(os.path.dirname(__file__), "data/raw")
-    recipes_df = df
-    recipes_df.rename(columns={"id": "recipe_id"}, inplace=True)
     reviews_df = pd.read_csv(f'{csv_path}/RAW_interactions.csv')
-    merged_df = recipes_df.merge(reviews_df, on="recipe_id", how="inner")
-    review_merge_df = merged_df.groupby(by="name") \
-                               .agg({"recipe_id": "first", "minutes": "first", "contributor_id": "first", \
-                                     "submitted": "first", "tags": "first", "nutrition": "first", "steps": "first", \
-                                     "description": "first", "ingredients": "first", "n_ingredients": "first", \
-                                     "rating": "mean"}).dropna()
-    #removing very specific ingredients
-    #1st i
-    print("creating lists for filtering ......")
+    merged_df = df.merge(reviews_df, on="recipe_id", how="inner")
+    agg_df = merged_df.groupby(by="recipe_id").agg({"rating": ["mean", "count"]}).xs('rating', axis=1, drop_level=True)
+    review_merge_df = df.merge(agg_df, on="recipe_id", how="inner").dropna()
+
+    # Listing very specific ingredients
+    print("> Creating lists for filtering...")
     ingredients = review_merge_df["ingredients"]
-    # ingredients_list_clean = []
-    # ingredients_list_for_counting = []
-    # for i in ingredients:
-    #     for j in i:
-    #         if j not in ingredients_list_clean:
-    #             ingredients_list_clean.append(j)
-    #         ingredients_list_for_counting.append(j)
 
     ingredients_dict = {}
     for i in ingredients:
@@ -56,56 +51,65 @@ def select_universe(df):
             else:
                 ingredients_dict[j] += 1
 
-    # now the lowest occurring ingredients are found and assumed as "too specific"
-    # Recipes containing these ingredients will be removed
-    print("removing too specific ingredients now ......")
+    # Recipes containing too specific ingredients will be removed
+    low_use_ingredients = [i for i, count in ingredients_dict.items() if count < 2]
+    # print(f"> Removing rows with too specific ingredients now ({len(low_use_ingredients)})...")
+    # review_merge_df = __remove_list_from_df(review_merge_df, low_use_ingredients, "ingredients")
 
-    low_use_ingredients = [i for i, count in ingredients_dict.items() if count >= 2]
-    # low_use_ingredients = count_occurrence(ingredients_list_clean,ingredients_list_for_counting,2)
+    # Removing drinks and icecream recipes using tags
+    drink_tags = ["cocktails", "punch", "non-alcoholic", "ice-cream", "brewing", "beverages", "smoothies"]
+    print(f"> Removing rows with drinks tags now ({len(drink_tags)})...")
+    review_merge_df = __remove_list_from_df(review_merge_df, drink_tags, "tags")
 
-    df_merged_ing = remove_list_from_df(review_merge_df, low_use_ingredients, "ingredients")
+    # Removing outliers by cooking time and number of ingredients
+    print("> Removing rows with outliers from dataframe now...")
+    review_merge_df = review_merge_df[(review_merge_df.minutes <= 300) & (review_merge_df.n_ingredients <= 20)]
 
-    print("specific ingredients are now removed")
+    return review_merge_df
 
-    #removing drinks and icecream recipes using tags:
-    print("removing drinks now ......")
 
-    drink_tags=['cocktails', 'punch', "non-alcoholic", "ice-cream", "brewing", "beverages", "smoothies"]
-    df_merged_ing_drink = remove_list_from_df(df_merged_ing, drink_tags, "tags")
-    print("drinks are now removed")
+def __remove_list_from_df(df, tag_list, column):
+    """ Removing row from a dataframe that contain items from a specific tag list """
+    data = df.copy()
+    data['str'] = data[column].map(lambda x: (' ').join(x))
 
-    # removing outliers by cooking time and number of ingredients
-    print("removing outliers from dataframe now...")
-    df_post_time = df_merged_ing_drink[df_merged_ing_drink.minutes < 300]
-    df_post_ings = df_post_time[df_post_time.n_ingredients <21]
-    print("Outliers removed")
+    # for tag in tag_list:
+    warnings.filterwarnings("ignore", 'This pattern has match groups')
+    j_tag_list = ('|').join(tag_list)
+    data = data[~data['str'].str.contains(j_tag_list, flags=re.IGNORECASE, regex=True)]
 
-    return df_post_ings
+    data.drop(columns='str', inplace=True)
+    return data
 
-def count_occurrence(list_clean,list_tot, threshold):
-    # creating a list of items that only occur in a list x times (threshold)
-    excluding_list =[]
-    for i in list_clean:
-        count = 0
-        for j in list_tot:
-            if j == i:
-                count+=1
-        if count < threshold:
-            excluding_list.append(i)
-    return excluding_list
 
-def remove_list_from_df(df, tag_list, column):
-    #removing roew from a dataframe that contain items from a specific tag list
+def clean_data(df):
+    """ Cleaning and converting data for recipes """
+    print('Cleaning and converting data for recipes...')
 
-    to_remove = []
-    for tag in tag_list:
-        for index, row in df.iterrows():
-            if tag in row[column]:
-                to_remove.append(index)
+    # Generating nutrition columns
+    df[NUTRITION_COLS] = __clean_nutrition(df)
 
-    import ipdb; ipdb.set_trace()
+    # Stringifying list columns
+    for col in LIST_COLS:
+        if col != 'nutrition':
+            df[f"{col}_str"] = df[col].map(lambda x: (', ').join(x))
 
-    return df[df.loc[index, 'recipe_id'].isin(set(to_remove))]
+    # Converting date columns
+    df['submitted'] = pd.to_datetime(df['submitted'])
+
+    # Creating metadata column
+    df['metadata'] = df['name'] + " " + df['tags_str'] + " " + df['ingredients_str'] # + " " + df['steps_str'] + " " + df['description_str']
+
+    # Renaming contributor_id to user_id for coherence
+    df.rename(columns = {'contributor_id': 'user_id', 'mean': 'rating_mean', 'count': 'rating_count'}, inplace = True)
+
+    # Reorganizing column position (note: nutrition list column is dropped)
+    ordered_cols = ['recipe_id', 'user_id', 'name', 'rating_mean', 'rating_count', 'minutes', 'n_steps',
+                    'n_ingredients', 'calories', 'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat',
+                    'carbohydrates', 'tags', 'ingredients', 'steps', 'description', 'metadata', 'submitted']
+
+    target_df = pd.DataFrame(df, columns=ordered_cols)
+    return target_df
 
 
 def __clean_nutrition(df, col='nutrition'):
@@ -120,41 +124,17 @@ def __clean_nutrition(df, col='nutrition'):
     return df[NUTRITION_COLS]
 
 
-def clean_data(df):
-    """ Cleaning and converting data for recipes """
-    print('Cleaning and converting data for recipes...')
-
-    # Generating nutrition columns
-    df[NUTRITION_COLS] = __clean_nutrition(df)
-
-    # Stringifying list columns
-    for col in LIST_COLS:
-        if col != 'nutrition':
-            df[col] = df[col].map(lambda x: (', ').join(x))
-
-    # Converting date columns
-    df['submitted'] = pd.to_datetime(df['submitted'])
-
-    # Creating metadata column
-    df['metadata'] = df['tags'] + " " + df['ingredients'] + " " + df['steps'] + " " + df['description']
-
-    # Renaming contributor_id to user_id for coherence
-    df.rename(columns = {'contributor_id': 'user_id'}, inplace = True)
-
-    # Reorganizing column position (note: nutrition list column is dropped)
-    ordered_cols = ['recipe_id', 'user_id', 'name', 'minutes','n_steps', 'n_ingredients', 'calories',
-                    'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat', 'carbohydrates',
-                    'tags', 'ingredients', 'steps', 'description', 'metadata', 'submitted']
-
-    target_df = pd.DataFrame(df, columns=ordered_cols)
-    return target_df
-
-
 def get_data():
     """ Initial cleaning of recipe data """
     print('\n*** Initial cleaning of recipe data ***')
 
     return clean_data(select_universe(get_raw_data()))
+
+
+def generate_sample_data(size=2000):
+    recipes = get_data()
+    recipes_sample = recipes.sample(size)
+    reviews_sample = reviews.get_data(recipes_sample)
 
 
 if __name__ == "__main__":
