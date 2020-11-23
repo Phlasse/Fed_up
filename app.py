b4892189e19de5cd6fafc6272075b074e2569fe2
Olivier Paulo <op.olivierpaulo@gmail.com>
2020-11-23 19:14:50 +0000

    testing page navigation

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 """Frameworks for running multiple Streamlit applications as a single app.
 """
+
+import pandas as pd
 import streamlit as st
+
 import welcome
-import recommendations
+import recommendation
+
+@st.cache
+def load_result():
+    data = pd.read_csv("../Fed_up/data/preprocessed/data_preprocessed_recipe_pp_20201117_1347.csv")
+    return data
 
 
 class MultiApp:
@@ -23,20 +31,28 @@ class MultiApp:
             "title": title,
             "function": func
         })
+        return 0
 
-    def run(self):
+    def run(self, page='Main', arguments=''):
 
-        app = st.sidebar.radio(
-            'Go To',
-            self.apps,
-            format_func=lambda app: app['title'])
+        for app in self.apps:
+            if app['title'] == page:
+                if arguments != '':
+                    app['function'](arguments)
 
-        app['function']()
+                else:
+                    app['function']()
+        #function()
+        # app = st.sidebar.radio(
+        #     'Go To',
+        #     self.apps,
+        #     format_func=lambda app: appRecommendation['title'])
+        return 0
 
 
 if __name__ == "__main__":
 
     app = MultiApp()
     app.add_app("Main", welcome.main)
-    app.add_app("Recommendations", recommendations.main)
+    #app.add_app("Recommendation", recommendation.reco)
     app.run()
