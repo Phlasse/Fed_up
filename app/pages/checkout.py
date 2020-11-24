306744f1928e0933be75eaa98ada03a51bdcad3d
Phillip Zastrow <zastrow.phillip@gmail.com>
2020-11-24 16:15:18 +0000

    added checkout stuff

--- app/pages/checkout.py
+++ app/pages/checkout.py
@@ -15,6 +15,38 @@ import os
 
 def run(app):
     # Display headers
+    checkout_ids = app.user_checkouts
+    selected_recipe_ids=[236834, 265381, 283173]
+    recipe_df = app.recipes
+    selected_recipes_df = recipe_df[recipe_df.recipe_id.isin(selected_recipe_ids)]
     st.write("# Checkout")
     st.write(f"🛒 Confirm and export your selected dishes.")
     st.markdown("---")
+    summed_ingredients = {}
+
+    st.header("Session's recipes for checkout:")
+    #selection_ingredients =
+    #st.write(selected_recipes_df)
+    for i in range(len(selected_recipes_df["recipe_id"])):
+        st.write("*", list(selected_recipes_df.name)[i].capitalize())
+        temp_ingredients = list(selected_recipes_df.ingredients)[i]#.split("',")
+        for j in temp_ingredients.split("',"):
+            ji = j.replace("[", "").replace("]", "").replace("'", "")
+            if ji in summed_ingredients:
+                summed_ingredients[ji] += 1#*persons
+            else:
+                summed_ingredients[ji] = 1#*persons
+    st.header("Here is your grocery list:")
+    ing_list_exp = pd.DataFrame()
+    ing_list_exp["Ingredient"] = list(summed_ingredients.keys())
+    ing_list_exp["Quantity"] = list(summed_ingredients.values())
+    ###########################
+    ### weird export option ###
+    ###########################
+
+    txt = ing_list_exp.to_csv(index = False)
+    b64 = base64.b64encode(txt.encode()).decode()
+    href = f'<a href="data:file/ingredients.txt;base64,{b64}">Download</a> (right-click and save as Grocery.txt)'
+    st.markdown(href, unsafe_allow_html=True)
+    st.table(ing_list_exp)
+
